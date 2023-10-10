2dab06e9bb4746fb861dbe84bf8fbef1ba14c1eb
Emre Eser <emre.eser@sabanciuniv.edu>
2023-10-10 21:45:11 +0300

    affine lib functional

--- affine/affine_demo.py
+++ affine/affine_demo.py
@@ -1,10 +1,25 @@
+#!/bin/python3
 from typing import Tuple, List
+from random import randint, Random, seed
 from affine_lib import *
-from sys import argv, argc
+from sys import argv
 
-def __main__():
-    ptext :str = 'abcd'
-    key :Tuple[int, int] = (3, 7)
-    rsize: int = 26
-    ctext = enc(key, ptext, rsize) 
-    print(ctext)
+ptext : str = 'abcd'
+key : Tuple[int, int] = (3, 7)
+rsize = 26
+
+key : Tuple[int, int] = (Random().randint(1, 25), Random().randint(1, 25)) # 1 - 25: 1 to prevent it from becoming a shift cipher
+
+print("key =",key)
+
+for i in range(rsize):
+    test_ch = chr( i + ord('a'))
+
+    try:
+        assert(test_ch == dec(key, enc(key, test_ch, rsize), rsize))
+    except AssertionError:
+        print("ch = ", test_ch, i)
+        exit(1)
+
+print("all test passed!")
+exit(0)
--- affine/affine_lib.py
+++ affine/affine_lib.py
@@ -1,23 +1,24 @@
-from math import pow
+from math import pow as mpow # math pow and builtin pow are different
 from typing import List, Tuple
 
 def alph_ord(ch : str):
-    return ch - 'a'
-
+    return ord(ch) - ord('a')
+    
 def alph_ord_cap(ch : str):
-    return ch - 'A'
-
+    return ord(ch) - 'A'
+    
 
 def enc(key : Tuple[int, int], ptext : str, ring_size : int):
     ctext : str = ''
 
-    for i in len(ptext):
-        ctext += ((  (key[0] * alph_ord(ptext)) % ring_size + (key[1]) ) % ring_size)
+    for i in range(len(ptext)):
+        ctext += chr( ord('a') + ((  (key[0] * alph_ord(ptext[i])) % ring_size + (key[1]) ) % ring_size) )
     
     return ctext
 
 def dec(key : Tuple[int, int], ctext : str, ring_size: int ):
     ptext : str = ''
 
-    for i in len(ctext):
-        ptext += (  ((ctext - tuple[1]) % ring_size) * (pow(key[0], -1, ring_size))  ) % ring_size
+    for i in range(len(ctext)):
+        ptext += chr(((  ((alph_ord(ctext[i]) - key[1]) % ring_size) * (pow(key[0], -1, ring_size))  ) % ring_size) + ord('a') )
+    return ptext
